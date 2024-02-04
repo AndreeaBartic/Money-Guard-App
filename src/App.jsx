8aac904d92b1adff053b4f434f4625dd6de046b1
AndreeaBartic <andreeabartic20@gmail.com>
2024-02-04 11:50:50 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import PostsList from './components/PostsList';
-import AddPostForm from 'components/AddPostForm';
 export const App = () => {
-  return (
-    <>
-      <AddPostForm />
-      <PostsList />
-    </>
-  );
+  return <>home</>;
 };
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import postsReducer from './components/postsSlice';
-import usersReducer from './components/users/usersSlice';
-export const store = configureStore({
-  reducer: {
-    posts: postsReducer,
-    users: usersReducer,
-  },
-});
