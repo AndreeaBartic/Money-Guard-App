cdfe271d9ac589e3da9549c98c786ba5e638383e
AndreeaBartic <andreeabartic20@gmail.com>
2024-02-05 14:27:45 +0200

    merge undo

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,19 @@
 // AppRouter.js
+
 import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-// const HomePage = lazy(() =>
-//   import('../components/DesignContainer/DesignContainer')
-// );
+const Home = lazy(() => import('components/Home.jsx'));
 const RegistrationPage = lazy(() =>
   import('../Auth/RegistrationPage/RegistrationPage')
 );
-const LoginPage = lazy(() => import('../Auth/LoginPage/LoginPage.jsx'));
+const LoginPage = lazy(() => import('../Auth/LoginPage/LoginPage'));
 
 function AppRouter() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        {/* <Route path="/Money-Guard-App" element={<HomePage />} /> */}
+        <Route path="/Money-Guard-App" element={<Home />} />
         <Route
           path="/Money-Guard-App/register"
           element={<RegistrationPage />}
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import ButtonAddTransactions from 'components/ButtonAddTransactions';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <ButtonAddTransactions />
+    </div>
+  );
+};
+export default Home;
