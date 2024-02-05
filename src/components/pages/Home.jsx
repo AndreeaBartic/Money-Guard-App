e698419a7f9ca197e5c5bcf168d45b08965e1f31
AndreeaBartic <128624386+AndreeaBartic@users.noreply.github.com>
2024-02-05 20:54:53 +0200

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { DesignContainer } from 'components/DesignContainer/DesignContainer';
 import { Container } from '../Container/Container';
-import ButtonAddTransactions from 'components/BuButtonAddTransactions'
+import ButtonAddTransactions from 'components/ButtonAddTransactions'
 import React from 'react';
 
 const Home = () => {
