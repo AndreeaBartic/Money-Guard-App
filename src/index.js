e43ceef3e4e8a85014438de2a99d009bb723dd7b
AndreeaBartic <andreeabartic20@gmail.com>
2024-02-08 12:17:43 +0200

    err

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
+import { persistor, store } from './Redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { HelmetProvider } from 'react-helmet-async';
@@ -25,4 +25,3 @@ root.render(
     </HelmetProvider>
   </React.StrictMode>
 );
-
