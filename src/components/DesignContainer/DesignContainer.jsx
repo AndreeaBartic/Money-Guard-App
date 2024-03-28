cf2196d9e49bde7953e8ee58c408f69a94f9d578
AndreeaBartic <128624386+AndreeaBartic@users.noreply.github.com>
2024-03-28 19:41:10 +0200

    Update DesignContainer.jsx

--- src/components/DesignContainer/DesignContainer.jsx
+++ src/components/DesignContainer/DesignContainer.jsx
@@ -1,7 +1,6 @@
 import { Header } from '../Header/Header';
 import { Loader } from '../Loader/Loader';
 import { Sidebar } from '../Sidebar/Sidebar';
-import { TransactionCard } from '../../components/pages/TransactionCardMobile/TransactionCard';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Background, Container, DashboardPage } from './DesignContainer.styled';
